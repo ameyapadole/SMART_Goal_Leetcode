4edcc389c93de88f90b99e5b8c4a79d759d4674f
Ameya Padole <ameya@uhtasi.local>
2025-01-27 11:57:46 -1000

    Jan 27 2025

--- 20_valid_parenthesis.py
+++ 20_valid_parenthesis.py
@@ -13,3 +13,9 @@ def validParenthesis(s):
     return not stack
 
 print(validParenthesis("()[]{}"))
+
+
+"""
+Time Complexity = O(N) because we iterate through the string on char at a time and the push pop operations takes O(1) time.
+Space Complexity = O(N) because we push all opening brackets onto the stack, the worst case would be {{{{{{{
+"""
